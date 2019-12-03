842571472247d2d9fcb73257a4f6aa401fcbe926
Dhileep Kumar P <DhileepKumar.P@mrcooper.com>
2019-12-04 02:45:40 +0530

    adding outputs.tf

--- modules/aws/outputs.tf
+++ modules/aws/outputs.tf
@@ -2,3 +2,23 @@
 output "ssh_user" {
   value = "ubuntu"
 }
+
+output "manager-instance_id" {
+  description = "List of IDs of manager instances"
+  value       = aws_instance.swarm_manager.*.id
+}
+
+output "worker-instance_id" {
+  description = "List of IDs of worker instances"
+  value       = aws_instance.swarm_worker.*.id
+}
+
+output "manager_public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = aws_instance.swarm_manager.*.public_ip
+}
+
+output "worker_public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = aws_instance.swarm_worker.*.public_ip
+}
--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,23 @@
 output "ssh_user" {
   value = "${module.terraform-ansible-docker.ssh_user}"
 }
+
+output "manager-instance_id" {
+  description = "List of IDs of manager instances"
+  value       = "${module.terraform-ansible-docker.manager-instance_id}"
+}
+
+output "worker-instance_id" {
+  description = "List of IDs of worker instances"
+  value       = "${module.terraform-ansible-docker.worker-instance_id}"
+}
+
+output "manager_public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = "${module.terraform-ansible-docker.master_public_ip}"
+}
+
+output "worker_public_ip" {
+  description = "List of public IP addresses assigned to the instances, if applicable"
+  value       = "${module.terraform-ansible-docker.worker_public_ip}"
+}
